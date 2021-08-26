f571ff3cbdbfcfea5993a1b30e7e8de75eace298
LV <vvvlizagub@gmail.com>
2021-08-26 13:51:10 +0300

    Initial Commit

---
+++ Task5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task5
+//
+//  Created by neoviso on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
